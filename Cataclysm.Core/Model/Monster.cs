ccc1883ce89470e807446618bf249de0ac5da8b4
ashes999 <ashes999@yahoo.com>
2017-02-01 16:55:43 -0500

    Some tweaks to model; new MonsterPrototype class

--- Cataclysm.Core/Model/Monster.cs
+++ Cataclysm.Core/Model/Monster.cs
@@ -1,17 +1,16 @@
 namespace DeenGames.Cataclysm.Core.Model
 {
-    class Monster
+    /// <summary>
+    /// An actual, real monster that the player fights or owns. With its genes (eg. red eyes).
+    /// </summary>
+    class Monster : MonsterPrototype
     {
         // unique name given by the player
         public string Name { get; set; }
 
-        // eg. Lion
-        public string Type {get; private set; }
-        
-        public Monster(string name, string type)
+        public Monster(string name, string type) : base(type)
         {
             this.Name = name;
-            this.Type = type;
         }
     }
 }
